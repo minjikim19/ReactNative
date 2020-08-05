39ba3c70beab8bb6822f3c2231490d8c43f85f39
Minji Kim <minjikm19@gmail.com>
2020-08-05 15:00:44 +0900

    Activity Indicator

--- conFusion/components/MenuComponent.js
+++ conFusion/components/MenuComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { View, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import { DISHES } from "../shared/dishes";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
@@ -34,13 +35,23 @@ class Menu extends Component {
       );
     };
 
-    return (
-      <FlatList
-        data={this.props.dishes.dishes}
-        renderItem={renderMenuItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    );
+    if (this.props.dishes.isLoading) {
+      return <Loading />;
+    } else if (this.props.dishes.errMess) {
+      return (
+        <View>
+          <Text>{props.dishes.errMess}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.dishes.dishes}
+          renderItem={renderMenuItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      );
+    }
   }
 }
 
--- conFusion/shared/baseUrl.js
+++ conFusion/shared/baseUrl.js
@@ -1,1 +1,2 @@
+/* change it to your IP adress */
 export const baseUrl = "http://172.30.1.49:3001/";
