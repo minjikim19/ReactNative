b4b2041038943132630ffe845a130d9f4f682ad9
Minji Kim <minjikm19@gmail.com>
2020-08-15 00:46:29 +0900

    Persist Redux Store

--- conFusion/redux/configureStore.js
+++ conFusion/redux/configureStore.js
@@ -6,10 +6,20 @@ import { comments } from "./comments";
 import { promotions } from "./promotions";
 import { leaders } from "./leaders";
 import { favorites } from "./favorites";
+import { persistStore, persistCombineReducers } from "redux-persist";
+import { AsyncStorage } from "react-native";
+
+const config = {
+  key: "root",
+
+  storage: AsyncStorage,
+
+  debug: true,
+};
 
 export const ConfigureStore = () => {
   const store = createStore(
-    combineReducers({
+    persistCombineReducers(config, {
       dishes,
       comments,
       promotions,
@@ -19,5 +29,7 @@ export const ConfigureStore = () => {
     applyMiddleware(thunk, logger)
   );
 
-  return store;
+  const persistor = persistStore(store);
+
+  return { persistor, store };
 };
