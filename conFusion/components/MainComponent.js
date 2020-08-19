31cc29b5958870817db787db23f9aaaf05e7cdda
Minji Kim <minjikm19@gmail.com>
2020-08-20 02:19:25 +0900

    Secure Store

--- conFusion/components/MainComponent.js
+++ conFusion/components/MainComponent.js
@@ -15,6 +15,7 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import Reservation from "./ReservationComponent";
 import Favorites from "./FavoriteComponent";
+import Login from "./LoginComponent";
 import { connect } from "react-redux";
 import {
   fetchDishes,
@@ -236,6 +237,32 @@ function FavoritesNavigatorScreen() {
   );
 }
 
+const LoginNavigator = createStackNavigator();
+
+function LoginNavigatorScreen() {
+  return (
+    <LoginNavigator.Navigator
+      initialRouteName="Login"
+      screenOptions={HeaderOptions}
+    >
+      <LoginNavigator.Screen
+        name="Login"
+        component={Login}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name="menu"
+              size={24}
+              color="white"
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </LoginNavigator.Navigator>
+  );
+}
+
 const MainNavigator = createDrawerNavigator();
 
 function MainNavigatorDrawer() {
@@ -246,6 +273,20 @@ function MainNavigatorDrawer() {
         backgroundColor: "#D1C4E9",
       }}
     >
+      <MainNavigator.Screen
+        name="Login"
+        component={LoginNavigatorScreen}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name="sign-in"
+              type="font-awesome"
+              size={24}
+              color={tintColor}
+            />
+          ),
+        }}
+      />
       <MainNavigator.Screen
         name="Home"
         component={HomeNavigatorScreen}
